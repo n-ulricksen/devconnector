bc8dd2869338814e8d5e5732e0ab3bdfb4558558
Nick Ulricksen <n.ulricksen@gmail.com>
2018-05-26 19:37:07 -0700

    added post route for experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -23,6 +23,7 @@ router.get('/test', (req, res) => res.json({ msg: "Profiles Works" }));
 // @access  Private
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
   const errors = {};
+
   Profile.findOne({ user: req.user.id })
     .populate('user', ['name', 'avatar'])
     .then(profile => {
@@ -146,6 +147,37 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
             new Profile(profileFields).save().then(profile => res.json(profile));
           });
       }
+    });
+});
+
+// @route   POST api/profile/experience
+// @desc    Add experience to profile
+// @access  Private
+router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validateExperienceInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    // Return any errors with 400 status
+    return res.status(400).json(errors);
+  }
+
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      }
+
+      // Add to beginning of experience array
+      profile.experience.unshift(newExp);
+
+      profile.save().then(profile => res.json(profile));
     });
 });
 
