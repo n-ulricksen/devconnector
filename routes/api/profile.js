7ce01b4a9010280bbaf12cde7f32a9352de09479
Nicholas Ulricksen <nicholas@Nicholass-MacBook-Pro.local>
2018-05-26 22:11:14 -0700

    added Post model, create Post route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 // Load Validation
 const validateProfileInput = require('../../validation/profile');
 const validateExperienceInput = require('../../validation/experience');
+const validateEducationInput = require('../../validation/education');
 
 // Load Profile Model
 const Profile = require('../../models/Profile');
@@ -79,7 +80,7 @@ router.get('/handle/:handle', (req, res) => {
 router.get('/user/:user_id', (req, res) => {
   const errors = {};
 
-  Profile.findOne({ user: req.params.user_id })  // req.params.handle grabs the handle from the url (:handle)
+  Profile.findOne({ user: req.params.user_id })  // req.params.user_id grabs the user_id from the url (:handle)
     .populate('user', ['name', 'avatar'])
     .then(profile => {
       if (!profile) {
@@ -181,11 +182,11 @@ router.post('/experience', passport.authenticate('jwt', { session: false }), (re
     });
 });
 
-// @route   POST api/profile/experience
-// @desc    Add experience to profile
+// @route   POST api/profile/education
+// @desc    Add education to profile
 // @access  Private
-router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const { errors, isValid } = validateExperienceInput(req.body);
+router.post('/education', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validateEducationInput(req.body);
 
   // Check Validation
   if (!isValid) {
@@ -195,22 +196,75 @@ router.post('/experience', passport.authenticate('jwt', { session: false }), (re
 
   Profile.findOne({ user: req.user.id })
     .then(profile => {
-      const newExp = {
-        title: req.body.title,
-        company: req.body.company,
-        location: req.body.location,
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldofstudy: req.body.fieldofstudy,
         from: req.body.from,
         to: req.body.to,
         current: req.body.current,
         description: req.body.description
       }
 
-      // Add to exp array
-      profile.experience.unshift(newExp);
+      // Add to edu array
+      profile.education.unshift(newEdu);
 
       profile.save().then(profile => res.json(profile));
     });
 });
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      // Get remove index
+      const removeIndex = profile.experience
+        .map(item => item.id)
+        .indexOf(req.params.exp_id)
+
+      // Splice out of array
+      profile.experience.splice(removeIndex, 1);
+
+      // Save
+      profile.save().then(profile => res.json(profile))
+        .catch(err => res.status(404).json(err));
+    });
+});
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      // Get remove index
+      const removeIndex = profile.education
+        .map(item => item.id)
+        .indexOf(req.params.edu_id)
+
+      // Splice out of array
+      profile.education.splice(removeIndex, 1);
+
+      // Save
+      profile.save().then(profile => res.json(profile))
+        .catch(err => res.status(404).json(err));
+    });
+});
+
+// @route   DELETE api/profile/
+// @desc    Delete user and profile
+// @access  Private
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  
+  Profile.findOneAndRemove({ user: req.user.id })
+    .then(() => {
+      User.findOneAndRemove({ _id: req.user.id })
+        .then(() => res.json({ success: true }));
+    });
+});
 
 module.exports = router;
