55b35814ce734ff11709426929cc0443a06ec44f
Nick Ulricksen <n.ulricksen@gmail.com>
2018-06-21 13:37:09 -0700

    Prepared for deploy

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const path = require("path");
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
@@ -13,14 +14,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // DB Config
-const dbCloud = require('./config/keys').mongoURI;
-const dbLocal = 'mongodb://localhost/devconnector';
+const dbCloud = require("./config/keys").mongoURI;
+const dbLocal = "mongodb://localhost/devconnector";
 
 // Connect to MongoDB
-mongoose.connect(dbLocal)   // Connect to local DB if running
+mongoose
+  .connect(dbLocal) // Connect to local DB if running
   .then(() => console.log("Connected to local MongoDB"))
   .catch(() => {
-    mongoose.connect(dbCloud)
+    mongoose
+      .connect(dbCloud)
       .then(() => console.log("Connected to MLab MongoDB"))
       .catch(err => console.log(err));
   });
@@ -29,14 +32,23 @@ mongoose.connect(dbLocal)   // Connect to local DB if running
 app.use(passport.initialize());
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-const port = process.env.PORT || 5000;    // heroku or local:5000
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000; // heroku or local:5000
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
